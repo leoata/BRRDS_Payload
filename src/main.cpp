26654b4f95bbe66b33e478e8040f48908028adce
leoata <leonardo@atalla.org>
2021-11-16 17:49:07 -0500

    less sensors. it actually works!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,10 @@
 #include <Adafruit_MPRLS.h>
 #include <Wire.h>
 #include "SD.h"
-#include "PWFusion_TCA9548A.h"
-#include <avr/wdt.h>
 
 Adafruit_ICM20649 icm;
 Adafruit_Sensor *icm_temp, *icm_accel, *icm_gyro;
-TCA9548A i2cMux;
-Adafruit_MPRLS mpr1 = Adafruit_MPRLS();
-Adafruit_MPRLS mpr3 = Adafruit_MPRLS();
-Adafruit_MPRLS mpr7 = Adafruit_MPRLS();
+Adafruit_MPRLS mpr = Adafruit_MPRLS();
 
 File sd;
 String fileName;
@@ -21,16 +16,7 @@ void setup(void)
     Serial.begin(9600);
     Wire.begin();
 
-    i2cMux.begin(4);
-    delay(100);
-    i2cMux.setChannel(CHAN1);
-    mpr1.begin();
-    i2cMux.setChannel(CHAN3);
-    mpr3.begin();
-    i2cMux.setChannel(CHAN7);
-    mpr7.begin();
-
-    i2cMux.setChannel(CHAN5);
+    mpr.begin();
     icm.begin_I2C();
 
     // create name of file
@@ -43,8 +29,8 @@ void setup(void)
     // open and write headers
     SD.begin(8);
     sd = SD.open(fileName.c_str(), FILE_WRITE);
-    sd.write("time,temperature,accelx,accely,accelz,gyrox,gyroy,gyroz,pressurehPa1,pressurehPa3,pressurehPa7\n");
-    Serial.println("time,temperature,accelx,accely,accelz,gyrox,gyroy,gyroz,pressurehPa1,pressurehPa3,pressurehPa7\n");
+    sd.write("time,temperature,accelx,accely,accelz,gyrox,gyroy,gyroz,pressurehPa\n");
+    Serial.println("time,temperature,accelx,accely,accelz,gyrox,gyroy,gyroz,pressurehPa\n");
     // save and re-open
     sd.close();
     sd = SD.open(fileName.c_str(), FILE_WRITE);
@@ -52,24 +38,19 @@ void setup(void)
 
 String imu_loop();
 
-String mprls_loop(Adafruit_MPRLS* mpr, uint8_t chan);
+String mprls_loop(Adafruit_MPRLS *mpr);
 
 void loop()
 {
     String s;
     s.concat(millis());
     s.concat(",");
-    String mprls1 = mprls_loop(&mpr1, CHAN1);
-    String mprls3 = mprls_loop(&mpr3, CHAN3);
-    String mprls7 = mprls_loop(&mpr7, CHAN7);
+    String mprls1 = mprls_loop(&mpr);
 
     s.concat(imu_loop());
+
     s.concat(",");
     s.concat(mprls1);
-    s.concat(",");
-    s.concat(mprls3);
-    s.concat(",");
-    s.concat(mprls7);
     s.concat("\n");
     Serial.print(s);
     sd.write(s.c_str());
@@ -79,33 +60,30 @@ void loop()
 
 String imu_loop()
 {
- i2cMux.setChannel(CHAN5);
-
-   //   Get a new normalized sensor event
-   sensors_event_t accel;
-   sensors_event_t gyro;
-   sensors_event_t temp;
-   icm.getEvent(&accel, &gyro, &temp);
-   String res;
-   res.concat(temp.temperature);
-   res.concat(",");
-   res.concat(accel.acceleration.x);
-   res.concat(",");
-   res.concat(accel.acceleration.y);
-   res.concat(",");
-   res.concat(accel.acceleration.z);
-   res.concat(",");
-   res.concat(gyro.gyro.x);
-   res.concat(",");
-   res.concat(gyro.gyro.y);
-   res.concat(",");
-   res.concat(gyro.gyro.z);
-   return res;
+    //   Get a new normalized sensor event
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+    icm.getEvent(&accel, &gyro, &temp);
+    String res;
+    res.concat(temp.temperature);
+    res.concat(",");
+    res.concat(accel.acceleration.x);
+    res.concat(",");
+    res.concat(accel.acceleration.y);
+    res.concat(",");
+    res.concat(accel.acceleration.z);
+    res.concat(",");
+    res.concat(gyro.gyro.x);
+    res.concat(",");
+    res.concat(gyro.gyro.y);
+    res.concat(",");
+    res.concat(gyro.gyro.z);
+    return res;
 }
 
-String mprls_loop(Adafruit_MPRLS *mpr, uint8_t chan)
+String mprls_loop(Adafruit_MPRLS *mpr)
 {
-    i2cMux.setChannel(chan);
     float pressure_hPa = mpr->readPressure();
     return String(pressure_hPa);
 }
